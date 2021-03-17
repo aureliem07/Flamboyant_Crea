8857bf8cf8ae8f905eddcd5e303069e7da180963
aureliem07 <aureliemaillot07@gmail.com>
2021-03-17 17:24:50 +0100

    Add cart page.

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,41 +2,74 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProduitRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
-    private $session;
-
-    public function __construct(SessionInterface $session)
-    {
-        $this->session = $session;
-    }
     
     /**
-     * @Route("/cart", name="cart")
+     * @Route("/panier", name="cart_index")
      */
-    public function index(): Response
+    public function cart(SessionInterface $session, ProduitRepository $produitRepository)
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+        $panier = $session->get('panier', []);
+        $panierWithData = [];
+        foreach($panier as $id => $quantity){
+            $panierWithData[] = [
+                'product' => $produitRepository->find($id),
+                'quantity' => $quantity
+            ];
+        }
+        $total = 0;
+        
+        foreach($panierWithData as $item) {
+            $totalItem = $item['product']->getPrixUnitaire() * $item['quantity'];  
+            $total += $totalItem;          
+        }
+
+        return $this->render('cart/cart.html.twig', [
+            'items' => $panierWithData,
+            'total' => $total
         ]);
     }
 
-    /**
+     /**
      * @Route("/panier/add/{id}", name="cart_add")
      */
-    public function add($id)
+    public function add($id, SessionInterface $session)
     {
-        $panier = $this->session->get('panier', []);
+        $panier = $session->get('panier', []);
 
-        $panier[$id] = 1;
+        if(!empty($panier[$id])) {
+            $panier[$id]++;
+        } else {
+            $panier[$id] = 1;
+        }
 
-        $session->set('panier',$panier);
+        $session->set('panier', $panier);
 
-        dd($session->get('panier'));
+        return $this->redirectToRoute("cart_index");
     }
+
+    /**
+     * @Route("/panier/remove/{id}", name="cart_remove")
+     */
+    public function remove($id, SessionInterface $session)
+    {
+        $panier = $session->get('panier', []);
+
+        if(!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute("cart_index");
+    }
+
 }
