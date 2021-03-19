59191c361c09a0279a4b343d3a98075ea7486059
aureliem07 <aureliemaillot07@gmail.com>
2021-03-19 11:08:04 +0100

    Cleanup migrations files.

--- migrations/Version20210319100513.php
+++ migrations/Version20210319100513.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210318115842 extends AbstractMigration
+final class Version20210319100513 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,9 @@ final class Version20210318115842 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, created_at DATETIME NOT NULL, INDEX IDX_F5299398A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE order_has_produits (id INT AUTO_INCREMENT NOT NULL, commande_id INT NOT NULL, produit_id INT NOT NULL, quantite INT NOT NULL, INDEX IDX_AB8BB1D282EA2E54 (commande_id), INDEX IDX_AB8BB1D2F347EFB (produit_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE order_has_produits ADD CONSTRAINT FK_AB8BB1D282EA2E54 FOREIGN KEY (commande_id) REFERENCES `order` (id)');
         $this->addSql('ALTER TABLE order_has_produits ADD CONSTRAINT FK_AB8BB1D2F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id)');
     }
@@ -28,6 +30,8 @@ final class Version20210318115842 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE order_has_produits DROP FOREIGN KEY FK_AB8BB1D282EA2E54');
+        $this->addSql('DROP TABLE `order`');
         $this->addSql('DROP TABLE order_has_produits');
     }
 }
