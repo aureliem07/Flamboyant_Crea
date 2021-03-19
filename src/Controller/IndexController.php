20c2756cd0635c2d497012f05f22579ff8dfcdb4
aureliem07 <aureliemaillot07@gmail.com>
2021-03-19 15:41:02 +0100

    Modify contact template to use API EmailJS.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -31,40 +31,9 @@ class IndexController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(Request $request, \Swift_Mailer $mailer)
-    {
-        $form = $this->createForm(ContactType::class);
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
-            $contact = $form->getData();
-
-            //ici nous enverons le mail
-            $message = (new \Swift_Message('Nouveau contact'))
-                //on attribue l'expéditeur
-                ->setFrom($contact['email'])
-
-                //on attribue le destinataire
-                ->setTo('monadressemail@gmail.com')
-
-                //on crée le message avec la vue Twig
-                ->setBody(
-                    $this->renderView(
-                        'emails/emailcontact.html.twig', compact('contact')
-                    ),
-                    'text/html'
-                )
-            ;
-
-            //on envoie le message
-            $mailer->send($message);
-
-            $this->addFlash('message', 'Le message a bien été envoyé.');
-            return $this->redirectToRoute('index');
-        }
-
+    public function contact(): Response
+    {   
         return $this->render('index/contact.html.twig', [
-            'contactForm' => $form->createView()
         ]);
     }
 }
